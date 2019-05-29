248706eb44e02e459d4cfc6a0fe853c5733b59e1
Leslie Wong <79917148leslie@gmail.com>
2019-05-30 00:35:43 +0800

    finish the deal

--- main/main.js
+++ main/main.js
@@ -1,4 +1,29 @@
+function getLess2() {
+    return 6;
+}
+
+function getOver2Less8(distance) {
+    if (distance <= 2) return 0;
+    if (distance <= 8) return 0.8 * (distance - 2);
+
+    return 0.8 * (8 - 2);
+}
+
+function getOver8(distance) {
+    if (distance <= 8) return 0;
+    return 0.8 * (1 + 0.5) * (distance - 8);
+}
+
+function getParkTime(parkTime) {
+    return 0.25 * parkTime
+}
+
+
 module.exports = function main(inputs) {
-    // write your code here...
-    return "price";
+    return Math.round(
+        getLess2() +
+        getOver2Less8(inputs.distance) +
+        getOver8(inputs.distance) +
+        getParkTime(inputs.parkTime)
+    );
 };
